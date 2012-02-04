00b6d2b2a51ed67a7d87cf9fc9f16d8422209d41
Lyle Underwood <lyle.underwood@bkwld.com>
2012-02-04 00:52:35 -0800

    octopress setup

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/notes/"
+http_images_path = "/notes/images"
+http_fonts_path = "/notes/fonts"
+css_dir = "public/notes/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
